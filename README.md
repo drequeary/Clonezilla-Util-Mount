59d78ec720bfa58bede61e9513c10dfe949f7082
DeAndre Queary <77317139+drequeary@users.noreply.github.com>
2023-11-28 09:51:35 -0600

    0.1.0 release

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Clonezilla-Util-Mount
  Interactive script for mounting Clonezilla images with Clonezilla Util.
+
+![screenshot](screenshot.png)
+
+# Requirements
+* Windows PowerShell
+* You MUST have [clonezilla-util](https://github.com/fiddyschmitt/clonezilla-util).
+* [Dokan driver](https://github.com/dokan-dev/dokany/releases/tag/v2.0.6.1000) is required for mounting disk images as folders or ISO files.
+
+# Installation
+1. Run `Set-ExecutionPolicy Unrestricted` as Admin in PowerShell to enable running .ps1 scripts.
+2. Copy `clonezilla-mount.ps1` into the `clonezilla-util` folder or whereever `clonezilla-util.exe` is located.
+3. Open `clonezilla-mount.ps1` in PowerShell.
--- clonezilla-mount.ps1
+++ clonezilla-mount.ps1
@@ -8,11 +8,11 @@
     Clonezilla Util.
 
 .CREDITS
-    Developer - DeAndre Wilson @DreQueary.
+    Developer - DeAndre Queary @drequeary.
     GitHub - https://github.com/drequeary
     Repository - https://github.com/drequeary/clonezilla-util-mount
     
-    Links:    
+    Links:
         Clonezilla-Util - https://github.com/fiddyschmitt/clonezilla-util
         Dokany Driver - https://github.com/dokan-dev/dokany
 
@@ -21,21 +21,28 @@
         Donkay Team - https://github.com/dokan-dev
 #>
 
-# Note: Set exe to path of clonezilla-util.exe if it's folder
-# isn't added to system PATH.
-$ClonezillaUtilExe = "clonezilla-util.exe"
-
 function Main
 {
     $Key = 0
-	
-    Clear-Host
-    Write-Host "Clonezilla Util Mount - v0.1.0 (2023)" -ForegroundColor Yellow
+    $MountType = $null
+	$ClonezillaUtilExe = "$PSScriptRoot\clonezilla-util.exe"
 
-    Write-Host "How do you want to mount Clonezilla image?" -ForegroundColor Cyan
-    Write-Host "1 - As Folder (Requires Dokan Driver)"
-    Write-Host "2 - Mount As Img Files (Requires Dokan Driver)"
-    Write-Host "3 - Extract Paritions To Img File"
+    Clear-Host
+    Write-Host "Clonezilla Util Mount by DeAndre Queary - v0.1.0" -ForegroundColor Yellow
+
+    if ($null -eq (Get-Command "$ClonezillaUtilExe" -ErrorAction SilentlyContinue)) {
+        Write-Host "clonezilla-util.exe wasn't found. Cannot start Clonezilla Mount." -ForegroundColor Red
+        Write-Host "Please place this script in the same folder as clonezilla-util.exe" -ForegroundColor DarkRed
+        Pause
+        EXIT
+    } else {
+        Write-Host "reset - enter [\] for input / exit - press [ESC] on main menu"
+    }
+    Write-Host "--------------------------------------------------------------" -ForegroundColor Green
+    Write-Host "How do you want to mount Clonezilla disk image?" -ForegroundColor Cyan
+    Write-Host "1 - Mount disk image as a folder. (Requires Dokan Driver)"
+    Write-Host "2 - Mount disk image as ISO files. (Requires Dokan Driver)"
+    Write-Host "3 - Export disk image partitions as ISO files."
 
     while ($Key -ne 49 -and $Key -ne 50 -and $Key -ne 51 -and $Key -ne 27 -and $Key -ne 97 -and $Key -ne 98 -and $Key -ne 99) {
         $Key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
@@ -43,22 +50,26 @@ function Main
         # 1 KEY
         if ($Key -eq 49 -or $Key -eq 97) 
         {
-            Write-Host "Mount Type: As Folder" -ForegroundColor Green
+            Write-Host "Mount Type: As Folder." -ForegroundColor Green
             $MountType = 1
         }
 
         # 2 KEY
         if ($Key -eq 50 -or $Key -eq 98) 
         {
-            Write-Host "Mount Type: As Img Files" -ForegroundColor Green
+            Write-Host "Mount Type: As ISO files." -ForegroundColor Green
             $MountType = 2
         }
 
         # 3 KEY
         if ($Key -eq 51 -or $Key -eq 99) 
         {
-            Write-Host "Mount Type: N/A (Extracting To Img File)" -ForegroundColor Green
+            Write-Host "Mount Type: N/A (Export partitions to ISO files)" -ForegroundColor Green
             $MountType = 3
+        }
+
+        if ($MountType -ge 1 -and $MountType -le 3) {
+            Start-ImgMount
         }
 
         # ESC KEY
@@ -71,46 +82,70 @@ function Main
             EXIT
         }
 
-        do {
-            $Image = Read-Host "[Enter Clonezilla Img]"
-        } while ($Image -eq "")
-    
-        if ($MountType -eq 3) {
-    
-            do {
-                $MountPath = Read-Host "[Enter Folder To Mount To]"
-            } while ($MountPath -eq "")
-    
-        } else {
-    
-            do {
-                $Drive = Read-Host "[Enter Letter Drive To Mount To]"
-            } while ($Drive -eq "")
-    
-        }
-    
-        $Image = $Image.Trim()
-        
-        Write-Host
-        if ($MountType -eq 1) {
-            Write-Host "Mounting image files to letter drive"$Drive -ForegroundColor Cyan
-            Start-Sleep 2
-    
-            & $ClonezillaUtilExe mount --input "$Image" --mount $Drive":\"
-        } elseif ($MountType -eq 2) {
-            Write-Host "Mounting disk images to letter drive"$Drive -ForegroundColor Cyan
-            Start-Sleep 2
-    
-            & $ClonezillaUtilExe mount-as-image-files --input "$Image" --mount $Drive":\"
-        } elseif ($MountType -eq 3) {
-            Write-Host "Extracting partition image to folder"$MountPath -ForegroundColor Cyan
-            Start-Sleep 2
-    
-            & $ClonezillaUtilExe extract-partition-image --input "$Image" --output "$MountPath"
-        }
-    
-        Pause
         Main
+    }
+}
+
+function Start-ImgMount
+{
+    Write-Host
+    Write-Host "Where is the disk image?" -ForegroundColor Cyan
+    do {
+        $Image = Read-Host "[Enter location (without qoutes)]"
+        Test-Reset -ReadInput $Image
+    } while ($Image -eq "")
+
+    if ($MountType -eq 3) {
+        Write-Host
+        Write-Host "Where to store extracted paritions?" -ForegroundColor Cyan
+        do {
+            $MountPath = Read-Host "[Enter save location (without qoutes)]"
+            Test-Reset -ReadInput $MountPath
+        } while ($MountPath -eq "")
+
+    } else {
+        Write-Host
+        Write-Host "What letter do you want to mount to?" -ForegroundColor Cyan
+
+        do {
+            $Drive = Read-Host "[Enter letter drive]"
+            Test-Reset -ReadInput $Drive
+        } while ($Drive -eq "")
+
+    }
+
+    $Image = $Image.Trim()
+
+    Write-Host
+    if ($MountType -eq 1) {
+        Write-Host "Mounting disk image to letter drive"$Drive -ForegroundColor Cyan
+        Start-Sleep 2
+
+        & "$ClonezillaUtilExe" mount --input "$Image" --mount $Drive":\"
+    } elseif ($MountType -eq 2) {
+        Write-Host "Mounting disk image as ISO files to letter drive"$Drive -ForegroundColor Cyan
+        Start-Sleep 2
+
+        & "$ClonezillaUtilExe" mount-as-image-files --input "$Image" --mount $Drive":\"
+    } elseif ($MountType -eq 3) {
+        Write-Host "Exporting disk image partitions as ISO files."$MountPath -ForegroundColor Cyan
+        Start-Sleep 2
+
+        & "$ClonezillaUtilExe" extract-partition-image --input "$Image" --output "$MountPath"
+    }
+
+    Pause
+}
+
+function Test-Reset
+{
+    param (
+        $ReadInput
+    )
+
+    if ($ReadInput -eq "\") {
+        Main
+        Return
     }
 }
 
